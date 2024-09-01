fb82400d4ee0b7e0e530805153443834c08d4440
gelo rosi <angelowither@gmail.com>
2024-09-01 14:18:38 +0800

    Add personalized greeting feature

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,10 @@
-console.log("Hello World (with change)");
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is your name? ', (name) => {
+    console.log(`Hello, ${name}!`);
+    rl.close();
+});
